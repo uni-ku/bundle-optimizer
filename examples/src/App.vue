e2d3bbcbe0cc58bdb101ad1bf20fdc2c81f526c9
Vanisper <273266469@qq.com>
2024-12-20 09:20:45 +0800

    🏀 examples: 创建 `example` 示例项目

---
+++ examples/src/App.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts">
+// eslint-disable-next-line no-console
+console.log('[uni-ku]', 'example init')
+</script>
---
+++ examples/src/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view class="center">
+    uniapp 分包优化
+  </view>
+</template>
+
+<style lang="css" scoped>
+.center {
+  color: red;
+  padding: 20px 0;
+  font-size: 20px;
+  font-weight: bolder;
+  text-align: center;
+}
+</style>
