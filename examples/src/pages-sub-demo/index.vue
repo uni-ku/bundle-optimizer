768cfce30334840be51f3b170aeaa3fe368681f4
Vanisper <273266469@qq.com>
2024-12-20 09:53:56 +0800

    🏀 examples: 初步完善示例项目，建立工作区，示例项目完成对工作区主项目(插件)的接入

---
+++ examples/src/pages-sub-demo/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>
+    <view class="center">
+      子包
+    </view>
+  </view>
+</template>
+
+<style lang="css" scoped>
+.center {
+  color: red;
+  padding: 20px 0;
+  font-size: 20px;
+  font-weight: bolder;
+  text-align: center;
+}
+</style>
--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,14 +1,20 @@
+/* eslint-disable ts/ban-ts-comment */
 import { fileURLToPath, URL } from 'node:url'
 import Uni from '@dcloudio/vite-plugin-uni'
+import Optimization from '@uni-ku/bundle-optimizer'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    Uni(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      Uni(),
+      // @ts-ignore
+      Optimization({ command, mode }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
