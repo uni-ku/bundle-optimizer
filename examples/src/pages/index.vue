8e51aa81bba78d53a631c4e848dd221ada49d3ca
Vanisper <273266469@qq.com>
2024-12-20 09:53:56 +0800

    examples: 🏀 初步完善示例项目，建立工作区，示例项目完成对工作区主项目(插件)的接入

--- examples/src/pages/index.vue
+++ examples/src/pages/index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-
+function goDemoSubPage() {
+  uni.navigateTo({
+    url: '/pages-sub-demo/index',
+  })
+}
 </script>
 
 <template>
   <view class="center">
     uniapp 分包优化
   </view>
+
+  <button @click="goDemoSubPage">
+    前往子包
+  </button>
 </template>
 
 <style lang="css" scoped>
