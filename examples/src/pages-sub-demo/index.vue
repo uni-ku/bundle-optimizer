b2e841ad0f2fcbc6f9490dcd8bae5f4356616950
Vanisper <273266469@qq.com>
2024-12-20 12:20:39 +0800

    🏀 examples: 完善示例项目，更新页面结构和配置

--- examples/src/pages-sub-demo/index.vue
+++ examples/src/pages-sub-demo/index.vue
@@ -1,8 +1,33 @@
+<!-- eslint-disable no-console -->
+<script lang="ts" setup>
+import SubComponent from '@/pages-sub-async/component.vue?async'
+import { onMounted } from 'vue'
+
+function goSubPage() {
+  uni.navigateTo({
+    url: '/pages-sub-async/index',
+  })
+}
+
+onMounted(() => {
+  AsyncImport('@/pages-sub-async/plugin').then((res) => {
+    res.AsyncPluginDemo().run()
+  })
+})
+</script>
+
 <template>
   <view>
     <view class="center">
-      子包
+      子包页面
     </view>
+    <view class="center lighter padding-0">
+      <text>------</text>
+      <SubComponent />
+    </view>
+    <button @click="goSubPage">
+      go go go
+    </button>
   </view>
 </template>
 
@@ -13,5 +38,17 @@
   font-size: 20px;
   font-weight: bolder;
   text-align: center;
+}
+
+.padding-0 {
+  padding: 0;
+}
+
+.small {
+  font-size: 12px;
+}
+
+.lighter {
+  font-weight: lighter;
 }
 </style>
