6871d2d0575d4cf53ad98c9bd28378ceff3d9c81
Vanisper <Vanisper@users.noreply.github.com>
2025-01-03 17:02:17 +0000

    deploy: 9903d551c2d70e7727a6f0696c229593b3bfc254

---
+++ assets/pages-sub-async-plugin.Ct_cpJfm.js
@@ -0,0 +1,1 @@
+import{s as n}from"./index-C-zI5nk3.js";function s(){return{name:"async-plugin",run(){console.log("[async-plugin]","run"),n({title:"异步插件执行✨",mask:!0,icon:"success"})}}}export{s as AsyncPluginDemo,s as default};
