8de1e4782121ceda07e115d75426a4a33899985e
Vanisper <273266469@qq.com>
2025-10-11 20:50:00 +0800

    fix(utils): 优化 vite 别名路径解析功能 #22

--- src/utils/getViteConfigPaths.ts
+++ src/utils/getViteConfigPaths.ts
@@ -4,12 +4,35 @@ import { isRegExp } from 'node:util/types'
 import { normalizePath } from '.'
 
 /**
+ * @link https://github.com/rollup/plugins/blob/c3dcdc0d2eda4db74bdc772bc369f3f9325802bf/packages/alias/src/index.ts#L7
+ */
+function matches(pattern: string | RegExp, source: string) {
+  if (pattern instanceof RegExp) {
+    return pattern.test(source)
+  }
+  if (source.length < pattern.length) {
+    return false
+  }
+  if (source === pattern) {
+    return true
+  }
+  return source.startsWith(pattern.endsWith('/') ? pattern : (`${pattern}/`))
+}
+
+/**
 /**
  * 创建一个基于 vite 配置的路径解析函数
  * @param config vite 配置
  * @returns 路径解析函数
  */
 export function createVitePathResolver(config: UserConfig) {
+  const normalize = (str: any) => {
+    if (typeof str === 'string' && !isRegExp(str) && !str.includes('*')) {
+      str = normalizePath(str)
+    }
+    return str
+  }
+
   const tempAlias = config.resolve?.alias ?? []
   let alias: Alias[] = []
   if (!Array.isArray(tempAlias)) {
@@ -20,29 +43,27 @@ export function createVitePathResolver(config: UserConfig) {
   }
 
   return (source: string, relative = false) => {
-    for (let { find, replacement, customResolver: _customResolver } of alias) {
-      if (!find || !replacement)
-        continue
+    const matchedEntry = alias.find(entry => matches(entry.find, source))
+    if (!matchedEntry) {
+      return source
+    }
+    const normalizeReplacement = normalize(matchedEntry.replacement)
 
-      source = normalizePath(source)
-      if (typeof replacement === 'string' && !isRegExp(replacement) && !replacement.includes('*')) {
-        replacement = normalizePath(replacement)
-      }
+    if (isRegExp(matchedEntry.find)) {
+      const realPath = source.replace(matchedEntry.find, normalizeReplacement)
+      return relative ? realPath : path.resolve(realPath)
+    }
 
-      if (!isRegExp(replacement) && typeof replacement === 'string' && !replacement.includes('*')
-        && !isRegExp(find) && typeof find === 'string' && !find.includes('*')) {
-        if (source === find) {
-          // 断定为全量匹配
-          return relative ? replacement : path.resolve(replacement)
-        }
-        else if (source.startsWith(find)) {
-          // 断定为前缀匹配
-          const realPath = source.replace(find, replacement)
-          return relative ? realPath : path.resolve(realPath)
-        }
+    // 避开 glob 特征的字符串
+    if (!matchedEntry.find.includes('*') && !normalizeReplacement.includes('*')) {
+      // 断定为全量匹配
+      if (source === matchedEntry.find) {
+        return relative ? normalizeReplacement : path.resolve(normalizeReplacement)
       }
-      else if (source.match(find) && (isRegExp(find) || !find.includes('*'))) {
-        const realPath = source.replace(find, replacement)
+      // 断定为前缀匹配
+      if (source.startsWith(matchedEntry.find)) {
+        const subPath = source.substring(matchedEntry.find.length) // 获取去除前缀的子串
+        const realPath = path.join(normalizeReplacement, subPath) // join 自动处理路径拼接问题
         return relative ? realPath : path.resolve(realPath)
       }
     }
