32a9140e32845943a66f1a7d225e14a01eeffaef
Vanisper <Vanisper@users.noreply.github.com>
2025-10-28 08:57:17 +0000

    deploy: 81251ae5e3680062cfeecd6c86d8fc38308ca998

---
+++ assets/pages-sub-async-plugin.dc782b2a.js
@@ -0,0 +1,1 @@
+import{p as n}from"./index-2fe02e49.js";function s(){return{name:"async-plugin",run(){console.log("[async-plugin]","run"),n({title:"异步插件执行✨",mask:!0,icon:"success"})}}}export{s as AsyncPluginDemo,s as default};
