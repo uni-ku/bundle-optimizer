548a6568b8a9ff90f64d0749a0e386999feb1339
Vanisper <273266469@qq.com>
2026-01-13 00:41:17 +0800

    feat(utils): 新增实现 isUniRenderDynamicImportOptions

--- src/utils/uniapp/is.ts
+++ src/utils/uniapp/is.ts
@@ -2,6 +2,11 @@ import type { RenderDynamicImportOptions } from '../../type'
 import type { UniRenderDynamicImportOptions } from './type'
 
 export function isUniRenderDynamicImportOptions(options: RenderDynamicImportOptions): options is UniRenderDynamicImportOptions {
-  // TODO:
-  return false
+  if (!('chunk' in options) || !('targetChunk' in options) || !('targetModuleId' in options) || !('getTargetChunkImports' in options)) {
+    return false
+  }
+  return typeof (options.chunk ?? {}) === 'object'
+    && typeof (options.targetChunk ?? {}) === 'object'
+    && typeof (options.targetModuleId ?? '') === 'string'
+    && typeof (options.getTargetChunkImports ?? (() => {})) === 'function'
 }
